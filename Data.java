476f097cd1967a2a064f60d520af4de972389bff
Sudip sah <35517366+sudipsah@users.noreply.github.com>
2018-04-19 09:41:14 +0600

    Add files via upload

--- Data.java
+++ Data.java
@@ -1,27 +1,52 @@
-package hell;
-import java.io.IOException;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
+import java.io.*;
+
+import java.util.ArrayList;
 
 public class Data {
-	
-	        public Data(String listOfLines) {
-	        	
-	        	try {
-	        		final Document document = Jsoup.connect("https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=+"+listOfLines).get();
 
-	            for (Element row: document.select("div.a-span7")) {
-	            	final String price2= row.select("span.a-offscreen").text();
-	            	System.out.println("Isbn number"+listOfLines+"\nPrice="+price2);
-	            
-	            }
-	        	}catch(IOException e) {
-	        		
-	        		e.printStackTrace();
-	        	}
-	          
-	        }
-	    }
+
+	public static void main(String args[]) throws IOException{
+		BufferedReader bufReader = new BufferedReader(new FileReader("/Users/sudipshah/eclipse-workspace/file.txt"));
+		ArrayList<String> listOfLines = new ArrayList<>(); 
+		String  line = bufReader.readLine();
+		while (line != null) { 	
+		listOfLines.add(line); line = bufReader.readLine(); 
+		}
+		System.out.println("Information from Abe.com\n");
+		String isbn1=listOfLines.get(0);
+		String isbn2=listOfLines.get(1);
+		String isbn3=listOfLines.get(2);
+		Abe value =new Abe();
+		value.Data1(isbn1);
+		value.Data2(isbn2);
+		value.Data3(isbn3);
+
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*
+		System.out.println("Information from Amazon.com\n");
+		String isbn4=listOfLines.get(3);
+		String isbn5=listOfLines.get(4);
+		String isbn6=listOfLines.get(5);
+		Data1 value1 =new Data1(isbn4);
+		value1.Data2(isbn5);
+		value1.Data3(isbn6);
+		*/
+		
+	}
+}
+
+
+
 
 
